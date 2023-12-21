c0485fa874dbed5a585a6a03fc41937c1dd919fe
wesclusa1980 <53847813+wesclusa1980@users.noreply.github.com>
2023-12-21 13:52:47 -0500

    x

--- client/declarations.d.ts
+++ client/declarations.d.ts
@@ -2,3 +2,8 @@ declare module '*.jpg';
 declare module '*.jpeg';
 declare module '*.png';
 declare module '*.svg';
+interface ImportMeta {
+    readonly env: {
+      [key: string]: string;
+    };
+  }
